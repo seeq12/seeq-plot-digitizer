1c282ffda03d53cd92c812b7ccddad2592237df3
Eric Parsonnet <e.parsonnet@berkeley.edu>
2022-09-06 10:23:24 -0400

    ya

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 version_scope = {'__builtins__': None}
-with open("seeq/addons/correlation/_version.py", "r+") as f:
+with open("seeq/addons/plot_digitizer/_version.py", "r+") as f:
     version_file = f.read()
     version_line = re.search(r"__version__ = (.*)", version_file)
     if version_line is None:
@@ -27,31 +27,24 @@ with open("seeq/addons/correlation/_version.py", "r+") as f:
         raise
 
 setup_args = dict(
-    name='seeq-correlation',
+    name='seeq-plot-digitizer',
     version=version_scope['__version__'],
-    author="Seeq Corporation",
-    author_email="applied.research@seeq.com",
+    author="Eric Parsonnet",
+    author_email="e.parsonnet@berkeley.edu",
     license='Apache License 2.0',
     platforms=["Linux", "Windows"],
-    description="Correlation analysis of time series data in Seeq",
+    description="Plot digitization in Seeq",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/seeq12/seeq-correlation",
+    url="https://github.com/eparsonnet93/seeq-plot-digitizer",
     packages=setuptools.find_namespace_packages(include=[namespace]),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'dask[complete]>=2021.10.0',
-        'ipyvuetify>=1.5.1',
-        'matplotlib>=3.1.3',
-        'memoization>=0.2.2',
-        'numpy>=1.19.5',
-        'pandas>=1.2.1',
-        'plotly>=4.5.0',
-        'python-dateutil>=2.8.1',
-        'scikit-learn>=0.22.1',
-        'seaborn>=0.10.0',
-        'sklearn>=0.0',
+        'bokeh>=2.4.2',
+        'imageio>=2.19.3',
+        'numpy>=1.22.0',
+        'pandas>=1.3.3'
     ],
     classifiers=[
         "Programming Language :: Python :: 3.7",
